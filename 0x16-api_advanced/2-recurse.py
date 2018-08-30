a6bc308817c5afacde671195fb48a8833102aa9e
Sonia Chevli <soniachevli@gmail.com>
2018-08-30 20:36:01 +0000

    first commit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+queries the Reddit API and
+returns the hot_list titles
+'''
+import requests
+
+
+def recurse(subreddit, hot_list=[], index=0):
+    headers = {
+        'User-Agent': 'My User Agent 1.0'
+    }
+    URL = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    try:
+        print('a')
+        reddit_data = requests.get(url=URL, headers=headers)
+        all_data = reddit_data.json().get('data').get('children')
+
+        if all_data[index].get('data').get('title'):
+            hot_list.append(all_data[index].get('data').get('title'))
+            return(recurse(subreddit, hot_list, index + 1))
+    except:
+        return hot_list
